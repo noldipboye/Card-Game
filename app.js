dda3e517874afe1a6d6652586970d3effd78afdd
Andrew <asjogren@leisdstudent.ws>
2022-04-14 10:33:51 -0500

    changed image size

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function button1() {
 
     getCard()
+    
 }
 
 function getCard() {
@@ -8,4 +9,12 @@ function getCard() {
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4)
     document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=Cards/" + r + suitArray[randomSuit] + ".jpg>")
+    document.getElementById("card").style.display = "flex"
+    
+    document.getElementById("card").style.justifyContent = "center"
+    
+    
+    return r
 }
+
+

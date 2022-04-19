8f51b130a0b85595c401e3976efa099c2a0a3408
Nolan Dipboye <noldipboye@leisdstudent.ws>
2022-04-19 10:42:11 -0500

    Added new row of cards

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 function button1() {
 
 
-   console.log(text())
+   text()
    
     
 }
 
 function button2() {
 
-    
+    text2()
+
 
 }
 
@@ -26,9 +27,9 @@ function computerCards() {
     var v = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4)
-    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=Cards/" + r + suitArray[randomSuit] + ".jpg>")
-    document.getElementById("card").style.display = "flex"
-    document.getElementById("card").style.justifyContent = "center"
+    document.getElementById("card2").insertAdjacentHTML("beforeend", "<img src=Cards/" + v + suitArray[randomSuit] + ".jpg>")
+    document.getElementById("card2").style.display = "flex"
+    document.getElementById("card2").style.justifyContent = "center"
     return v
 }
 
@@ -47,15 +48,34 @@ function limiter() {
     
 }
 
+function limiter2() {
+
+    if (math <= 21) {
+    m = computerCards()
+
+    math = (m + math)
+    }
+
+    return math
+    
+}
+
 function text() {
     let a = limiter()
     document.getElementById("yourNumber").innerHTML = "Your total is " + a + "."
     document.getElementById("yourNumber").style.display = "flex"
     document.getElementById("yourNumber").style.justifyContent = "center"
 
+}
 
+function text2() {
+    let b = limiter2()
+    document.getElementById("yourNumber").innerHTML = "The Computer's total is " + b + "."
+    document.getElementById("yourNumber").style.display = "flex"
+    document.getElementById("yourNumber").style.justifyContent = "center"
 
 }
+
 
 // Limiter
 
